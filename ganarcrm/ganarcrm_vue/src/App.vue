cc685a0db573aff6f54efea30c44bf5669b264ff
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-05-16 19:50:47 +0200

    first commit

---
+++ ganarcrm/ganarcrm_vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Navbar />
+
+    <section class="section">
+      <router-view/>
+    </section>
+  </div>
+</template>
+
+<script>
+  import Navbar from '@/components/layout/Navbar'
+  export default {
+      name: 'App',
+      components: {
+        Navbar
+      }
+  }
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+</style>
